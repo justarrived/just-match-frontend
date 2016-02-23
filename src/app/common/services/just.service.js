96cff92ee2726caa70ff320c4f07e40905270d4c
Niklas Granryd <niklas.granryd@gmail.com>
2016-02-23 19:01:55 +0100

    Fix more indent.

--- src/app/common/services/just.service.js
+++ src/app/common/services/just.service.js
@@ -1,10 +1,10 @@
 angular.module('just.service', [])
-    .service('i18nService', ['$translate','tmhDynamicLocale','settings', function($translate, tmhDynamicLocale, settings) {
-        this.useLanguage = function(code) {
-            $translate.use(code);
-            tmhDynamicLocale.set(code);
-        };
-        this.supportedLanguages = function() {
-            return settings.translated_languages;
-        };
-    }]);
+  .service('i18nService', ['$translate','tmhDynamicLocale','settings', function($translate, tmhDynamicLocale, settings) {
+    this.useLanguage = function(code) {
+      $translate.use(code);
+      tmhDynamicLocale.set(code);
+    };
+    this.supportedLanguages = function() {
+      return settings.translated_languages;
+    };
+  }]);
