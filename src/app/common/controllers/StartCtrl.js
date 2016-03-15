d38e72a65e115b381a1ea867e1f9375a3d1cebb4
Mikael Ek <mickeek@gmail.com>
2016-03-15 20:24:58 +0100

    WIP

--- src/app/common/controllers/StartCtrl.js
+++ src/app/common/controllers/StartCtrl.js
@@ -1,8 +1,12 @@
 angular.module('just.common')
-  .controller('StartCtrl', ['i18nService', function (i18nService) {
+  .controller('StartCtrl', ['i18nService', 'jobService', function (i18nService, jobService) {
     this.languages = i18nService.supportedLanguages();
+
+    this.jobs = jobService.getJobs();
+
     this.language = 'sv';
     this.selectLanguage = function () {
       i18nService.useLanguage(this.language);
     };
+
   }]);
