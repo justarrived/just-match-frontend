51fe784a38083b1cfc121b7eb8c4a5b6d2631126
Niklas Granryd <niklas.granryd@gmail.com>
2016-03-29 19:10:36 +0200

    Implemented create job flow.

--- src/app/common/directives/Job.js
+++ src/app/common/directives/Job.js
@@ -3,12 +3,18 @@ angular
   .directive('jobHeader', function() {
     return {
       restrict: 'E',
+      scope: {
+        model: '=model'
+      },
       templateUrl: 'common/templates/job-header.html'
     };
   })
   .directive('jobBody', function() {
     return {
       restrict: 'E',
+      scope: {
+        model: '=model'
+      },
       templateUrl: 'common/templates/job-body.html'
     };
   });
