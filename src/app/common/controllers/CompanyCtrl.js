a8bebfb75ce55654d7517fae586db5657fb98405
Wiratti Anunpiriyakul <wiratti.anunpiriyakul@se-production.com>
2016-04-21 11:19:14 +0700

    Add translation text and register service

--- src/app/common/controllers/CompanyCtrl.js
+++ src/app/common/controllers/CompanyCtrl.js
@@ -5,6 +5,11 @@ angular
      * @name just.common.controller:CompanyCtrl
      *
      */
-    .controller('RegisterCompanyCtrl', ['companyService', 'i18nService', function (companyService) {
+    .controller('RegisterCompanyCtrl', ['companyService', 'Resources', function (companyService) {
         var that = this;
+        this.data = companyService.signinModel;
+        this.message = companyService.signinMessage;
+        this.process = function () {
+            companyService.register(that.data);
+        };
     }]);
--- src/app/common/services/company-service.js
+++ src/app/common/services/company-service.js
@@ -9,4 +9,10 @@ angular.module('just.service')
     .service('companyService', ['$q', 'justFlowService', 'authService', 'Resources', 'justRoutes', 'i18nService', '$timeout', function ($q, flow, authService, Resources, routes, i18nService, $timeout) {
         var that = this;
 
+        this.registerModel = {language_id: i18nService.getLanguage().id};
+        this.registerMessage = {};
+
+        this.register = function (attributes) {
+            that.registerModel = attributes;
+        };
     }]);
