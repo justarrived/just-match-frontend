de3d6ea8764d2fa51936c9ebf292b6dc066e1512
Wiratti Anunpiriyakul <wiratti.anunpiriyakul@se-production.com>
2016-05-05 15:53:44 +0700

    Load company logo as job image

--- src/app/common/controllers/StartCtrl.js
+++ src/app/common/controllers/StartCtrl.js
@@ -1,102 +1,130 @@
 /*
+ angular.module('just.common')
+ .controller('StartCtrl', ['i18nService', 'jobService', function (i18nService, jobService) {
+ this.languages = i18nService.supportedLanguages();
+
+ this.jobs = jobService.getJobs();
+
+ this.language = 'sv';
+ this.selectLanguage = function () {
+ i18nService.useLanguage(this.language);
+ };
+
+ }]);
+
+ */
+
 angular.module('just.common')
-  .controller('StartCtrl', ['i18nService', 'jobService', function (i18nService, jobService) {
-    this.languages = i18nService.supportedLanguages();
+    .controller('StartCtrl', ['i18nService', 'jobService', '$scope', '$filter', '$q', 'Resources',
+        function (i18nService, jobService, $scope, $filter, $q, Resources) {
+            var that = this;
 
-    this.jobs = jobService.getJobs();
+            this.languages = i18nService.supportedLanguages();
 
-    this.language = 'sv';
-    this.selectLanguage = function () {
-      i18nService.useLanguage(this.language);
-    };
+            this.jobs = jobService.getJobs('company');
+            this.jobs.$promise.then(function (response) {
+                var deferd = $q.defer();
 
-  }]);
+                angular.forEach(response.data, function (obj, idx) {
+                    that.jobs.data[idx].company_image = "assets/images/content/placeholder-logo.png";
+                });
 
-*/
+                angular.forEach(response.data, function (obj, idx) {
 
-angular.module('just.common')
-    .controller('StartCtrl', ['i18nService', 'jobService', '$scope', function (i18nService, jobService, $scope) {
-        this.languages = i18nService.supportedLanguages();
+                    var found = $filter('filter')(response.included, {id: "" + obj.relationships.company.data.id}, true);
+                    if (found.length > 0) {
+                        if (found[0].relationships["company-images"].data.length > 0) {
+                            Resources.companyImage.get({
+                                company_id: found[0].id,
+                                id: found[0].relationships["company-images"].data[0].id
+                            }, function (result) {
 
-        this.jobs = jobService.getJobs();
+                                that.jobs.data[idx].company_image = result.data.attributes["image-url-small"];
+                            });
+                        }
+                    }
+                });
 
-        this.language = 'sv';
-        this.selectLanguage = function () {
-            i18nService.useLanguage(this.language);
-        };
+                return deferd.promise;
+            });
 
-        $scope.jobGalleryResponsive = [
-            {
-                breakpoint: 1920,
-                settings: {
-                    slidesToShow: 10,
-                    slidesToScroll: 10
-                }
-            },
-            {
-                breakpoint: 1600,
-                settings: {
-                    slidesToShow: 9,
-                    slidesToScroll: 9
-                }
-            },
-            {
-                breakpoint: 1440,
-                settings: {
-                    slidesToShow: 8,
-                    slidesToScroll: 8
-                }
-            },
-            {
-                breakpoint: 1280,
-                settings: {
-                    slidesToShow: 7,
-                    slidesToScroll: 7
-                }
-            },{
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 6,
-                    slidesToScroll: 6
-                }
-            },
-            {
-                breakpoint: 980,
-                settings: {
-                    slidesToShow: 5,
-                    slidesToScroll: 5
-                }
-            },
-            {
-                breakpoint: 767,
-                settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 4
-                }
-            },
-            {
-                breakpoint: 500,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3
-                }
-            },
-            {
-                breakpoint: 375,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            }
-        ];
+            this.language = 'sv';
+            this.selectLanguage = function () {
+                i18nService.useLanguage(this.language);
+            };
 
-        $scope.instructionsResponsive = [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 1
+            $scope.jobGalleryResponsive = [
+                {
+                    breakpoint: 1920,
+                    settings: {
+                        slidesToShow: 10,
+                        slidesToScroll: 10
+                    }
+                },
+                {
+                    breakpoint: 1600,
+                    settings: {
+                        slidesToShow: 9,
+                        slidesToScroll: 9
+                    }
+                },
+                {
+                    breakpoint: 1440,
+                    settings: {
+                        slidesToShow: 8,
+                        slidesToScroll: 8
+                    }
+                },
+                {
+                    breakpoint: 1280,
+                    settings: {
+                        slidesToShow: 7,
+                        slidesToScroll: 7
+                    }
+                }, {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 6,
+                        slidesToScroll: 6
+                    }
+                },
+                {
+                    breakpoint: 980,
+                    settings: {
+                        slidesToShow: 5,
+                        slidesToScroll: 5
+                    }
+                },
+                {
+                    breakpoint: 767,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 4
+                    }
+                },
+                {
+                    breakpoint: 500,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3
+                    }
+                },
+                {
+                    breakpoint: 375,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
                 }
-            }
-        ];
+            ];
 
-    }]);
+            $scope.instructionsResponsive = [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 1
+                    }
+                }
+            ];
+
+        }]);
