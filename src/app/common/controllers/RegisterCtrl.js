69050c6b873707ab823de12401103fd7d1966daf
Wiratti Anunpiriyakul <wiratti.anunpiriyakul@se-production.com>
2016-05-06 15:15:02 +0700

    Redirect to user profile when you already signed in

--- src/app/common/controllers/RegisterCtrl.js
+++ src/app/common/controllers/RegisterCtrl.js
@@ -1,28 +1,29 @@
 angular.module('just.common')
-    .controller('RegisterCtrl', ['authService', 'userService', 'justFlowService','justRoutes', function (authService, userService, flow,routes) {
-        var that = this;
+    .controller('RegisterCtrl', ['authService', 'userService', 'justFlowService', 'justRoutes',
+        function (authService, userService, flow, routes) {
+            var that = this;
 
-        if(authService.isAuthenticated()){
-            flow.redirect(routes.user.user.url);
-        }
-
-        this.data = userService.registerModel;
-        this.message = userService.registerMessage;
-
-        if (flow.next_data) {
-            this.data.company_id = flow.next_data.data.id;
-        }
-
-        this.process = function () {
-            var element0 = angular.element("#file_upload");
-            if (element0[0].files[0]) {
-                var formData = new FormData();
-                var element = angular.element("#file_upload");
-                formData.append("image", element[0].files[0]);
-                userService.register(that.data, formData);
-            } else {
-                userService.register(that.data);
+            if (authService.isAuthenticated()) {
+                flow.redirect(routes.user.user.url);
             }
 
-        };
-    }]);
+            this.data = userService.registerModel;
+            this.message = userService.registerMessage;
+
+            if (flow.next_data) {
+                this.data.company_id = flow.next_data.data.id;
+            }
+
+            this.process = function () {
+                var element0 = angular.element("#file_upload");
+                if (element0[0].files[0]) {
+                    var formData = new FormData();
+                    var element = angular.element("#file_upload");
+                    formData.append("image", element[0].files[0]);
+                    userService.register(that.data, formData);
+                } else {
+                    userService.register(that.data);
+                }
+
+            };
+        }]);
--- src/app/common/controllers/SigninCtrl.js
+++ src/app/common/controllers/SigninCtrl.js
@@ -1,10 +1,16 @@
 angular.module('just.common')
-  .controller('SigninCtrl', ['$scope', 'userService', function ($scope, userService) {
-      var that = this;
-      this.data = userService.signinModel;
-      this.message = userService.signinMessage;
-      this.process = function() {
-        userService.signin(that.data);
-      };
-    }]
-  );
+    .controller('SigninCtrl', ['$scope', 'authService', 'userService', 'justFlowService', 'justRoutes',
+        function ($scope, authService, userService, flow, routes) {
+            var that = this;
+
+            if (authService.isAuthenticated()) {
+                flow.redirect(routes.user.user.url);
+            }
+
+            this.data = userService.signinModel;
+            this.message = userService.signinMessage;
+            this.process = function () {
+                userService.signin(that.data);
+            };
+        }]
+    );
