8ebccf7ccb73d30c935cb6bb4b0c20fda737ee8b
Wiratti Anunpiriyakul <wiratti.anunpiriyakul@se-production.com>
2016-05-08 22:32:48 +0700

    Form register error message from api, fix 127

--- src/app/app.js
+++ src/app/app.js
@@ -19,7 +19,7 @@ angular.module('just', [
         'beauby.jsonApiDataStore',
         'just.translate',
         'slick',
-        'uiGmapgoogle-maps',
+        /*'uiGmapgoogle-maps',*/
         'AxelSoft',
         'oi.select'
     ])
@@ -283,10 +283,10 @@ angular.module('just', [
             .setPrefix('just-arrived')
             .setStorageType('sessionStorage');
     }])
-    .config(function (uiGmapGoogleMapApiProvider) {
+    /*.config(function (uiGmapGoogleMapApiProvider) {
         uiGmapGoogleMapApiProvider.configure({
             key: 'AIzaSyAQ-Iu3YFs_qXky2rAZNicY5gh6ampBq-M',
             v: '3.20',
             libraries: 'weather,geometry,visualization'
         });
-    });
+    })*/;
--- src/app/common/controllers/RegisterCtrl.js
+++ src/app/common/controllers/RegisterCtrl.js
@@ -1,6 +1,6 @@
 angular.module('just.common')
-    .controller('RegisterCtrl', ['authService', 'userService', 'justFlowService', 'justRoutes',
-        function (authService, userService, flow, routes) {
+    .controller('RegisterCtrl', ['authService', 'userService', 'justFlowService', 'justRoutes', '$scope',
+        function (authService, userService, flow, routes, $scope) {
             var that = this;
 
             if (authService.isAuthenticated()) {
@@ -13,6 +13,22 @@ angular.module('just.common')
             if (flow.next_data) {
                 this.data.company_id = flow.next_data.data.id;
             }
+
+            $scope.$watch('form', function(form) {
+                if(form) {
+                    if (that.message.data) {
+                        angular.forEach(that.message.data.errors, function (obj, key) {
+                            var pointer_arr = obj.source.pointer.split("/");
+                            var field_name = pointer_arr[pointer_arr.length - 1];
+                            field_name = field_name.replace(/-/g, "_");
+                            $scope.form[field_name].error_detail = obj.detail;
+                        });
+
+                    }
+                }
+            });
+
+
 
             this.process = function () {
                 var element0 = angular.element("#file_upload");
