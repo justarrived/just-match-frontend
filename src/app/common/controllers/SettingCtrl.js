f133647a48c46654b1f8bf0199a6bad335c7c775
Wiratti Anunpiriyakul <wiratti.anunpiriyakul@se-production.com>
2016-05-12 17:38:24 +0700

    Add Settings Content

--- src/app/common/controllers/SettingCtrl.js
+++ src/app/common/controllers/SettingCtrl.js
@@ -3,33 +3,16 @@ angular.module('just.common')
         function (flow, routes, authService, userService, $scope, Resources) {
             var that = this;
             this.languageList = "";
+            this.isCompany = userService.isCompany;
 
             $scope.model = userService.userModel();
 
 
             $scope.$on('onSignin', function (event) {
                 $scope.model = userService.userModel();
-                that.getLanguage();
             });
 
-            this.getLanguage = function () {
-                if (authService.isAuthenticated()) {
-                    Resources.userLanguage.get({
-                        user_id: authService.userId().id,
-                        'include': 'language'
-                    }, function (result) {
-                        that.languageList = "";
-                        angular.forEach(result.included, function (obj, idx) {
-                            if (that.languageList !== "") {
-                                that.languageList += ", ";
-                            }
-                            that.languageList += obj.attributes["en-name"];
-                        });
-                    });
-                }
-            };
 
-            this.getLanguage();
 
 
         }]);
