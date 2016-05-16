5794b42f5a43e0563489356ff63741cb5181b336
sirmackan <marcus@meeloo.se>
2016-05-16 20:13:14 +0200

    Added alternative to create a profile directly from the meny

--- src/app/common/controllers/MainCtrl.js
+++ src/app/common/controllers/MainCtrl.js
@@ -85,6 +85,7 @@ angular.module('just.common')
             this.showSetting = false;
             that.isCompany = -1;
             this.backStep = -1;
+            this.menuShowCreateAccount = false;
 
             this.signedIn = function () {
                 return authService.isAuthenticated();
@@ -126,6 +127,7 @@ angular.module('just.common')
                 //show = undefined : toggle
                 show = show | !routes.global.isMainMenuOpen;
                 routes.global.isMainMenuOpen = show;
+                this.menuShowCreateAccount = false;
             };
 
             this.setProfile = function () {
