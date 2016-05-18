8cbf0cbf2c7d5ed857956c57173390a7b52b4a16
Christopher Borghoff <christopher.borghoff@se-production.com>
2016-05-18 12:10:03 +0700

    format source code

---
+++ src/app/common/services/gt-service.js
@@ -0,0 +1,27 @@
+/**
+ * @ngdoc service
+ * @name just.service.service:commentService
+ * @description
+ * # commentService
+ * Service to handle comments.
+ */
+angular.module('just.service')
+    .service('commentService', ['Resources', 'i18nService', function (Resources, i18nService) {
+        var that = this;
+
+        this.getModel = function (resource_name, resource_id) {
+            return {
+                data: {
+                    attributes: {
+                        "commentable-id": resource_id,
+                        "commentable-type": resource_name
+                    }
+                }
+            };
+        };
+
+        this.getComments = function (resource_name, resource_id, include) {
+            return Resources.comments.get({resource_name: resource_name, resource_id: resource_id, 'include': include});
+        };
+
+    }]);
