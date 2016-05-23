bc968ce06b5f89214908e24ee6cd79e7955706d2
Wiratti Anunpiriyakul <wiratti.anunpiriyakul@se-production.com>
2016-05-23 11:13:58 +0700

    remove background-cover on directive

--- src/app/common/controllers/MainCtrl.js
+++ src/app/common/controllers/MainCtrl.js
@@ -85,8 +85,7 @@ angular.module('just.common')
         return function(scope, element, attrs){
             var url = attrs.backImg;
             element.css({
-                'background-image': 'url(' + url +')',
-                'background-size' : 'cover'
+                'background-image': 'url(' + url +')'
             });
         };
     })
