f47004b54d45da42107a8ecc20ce33a86565f407
Wiratti Anunpiriyakul <wiratti.anunpiriyakul@se-production.com>
2016-06-27 17:39:00 +0700

    issue 587 Show job detail

--- src/app/common/controllers/ArriverJobCtrl.js
+++ src/app/common/controllers/ArriverJobCtrl.js
@@ -149,8 +149,17 @@ angular.module('just.common')
 
             i18nService.addLanguageChangeListener(function () {
                     that.getChatMessage();
-                }
-            );
+                    that.getJobData();
+            });
+
+            //handle different dynamic translations
+            $scope.dt = {
+                job_item: true,
+                joblist_item: true
+            };
+            this.toggleDT = function (textId) {
+                $scope.dt[textId] = !$scope.dt[textId];
+            };
 
             Resources.arriverTermsAgreements.get(function (result) {
                 that.termsId = result.data.id;
@@ -211,19 +220,25 @@ angular.module('just.common')
             };
 
             this.getJobData = function () {
-                $scope.jobb = jobService.getJob($routeParams.id, 'company');
-                $scope.jobb.$promise.then(function (response) {
-                    $scope.job = response.data;
-                    $scope.job.company = response.included[0];
-                    that.canPerformed = that.checkJobDate(response.data.attributes["job-date"]);
+                Resources.job.get({
+                    id: $routeParams.id,
+                    "include": "owner,company,hourly-pay,job-users"
+                }, function (result) {
+                    $scope.isCompany = 0;
+                    $scope.job = result.data;
+                    $scope.job.owner = result.included[0];
+                    $scope.job.company = result.included[1];
+                    $scope.job.max_rate = result.included[2].attributes["gross-salary"];
+                    /*if ($scope.$parent) {
+                        $scope.job.max_rate = (($scope.$parent.ctrl.isCompany === 1) ? result.included[2].attributes["rate-excluding-vat"] : result.included[2].attributes["gross-salary"]);
+                    }*/
+                    $scope.job.totalRate = $scope.job.attributes.hours * $scope.job.max_rate;
+                    $scope.job.currency = result.included[2].attributes.currency;
+                    $scope.job.netRate = result.included[2].attributes["net-salary"] * $scope.job.attributes.hours;
                     $scope.job.company_image = "assets/images/content/placeholder-logo.png";
-
-                    $scope.comments_amt = response.data.relationships.comments.data.length;
-                    that.ratingModel.data.attributes["user-id"] = parseInt(response.data.relationships.owner.data.id);
-
-                    var company_image_arr = response.included[0].relationships["company-images"].data;
+                    var company_image_arr = result.included[1].relationships["company-images"].data;
                     if (company_image_arr.length > 0) {
-                        var getCompany = companyService.getCompanyById(response.included[0].id);
+                        var getCompany = companyService.getCompanyById(result.data.relationships.company.data.id);
                         if (getCompany) {
                             var found_image = $filter('filter')(getCompany.included, {type: 'company-images'}, true);
                             if (found_image) {
@@ -233,7 +248,7 @@ angular.module('just.common')
                             }
                         } else {
                             Resources.company.get({
-                                company_id: response.included[0].id,
+                                company_id: result.data.relationships.company.data.id,
                                 'include': 'company-images'
                             }, function (result0) {
                                 var found_image = $filter('filter')(result0.included, {type: 'company-images'}, true);
@@ -245,6 +260,37 @@ angular.module('just.common')
                                 companyService.addList(result0);
                             });
                         }
+                    }
+
+                    if (result.data.attributes.name) {
+                        gtService.translate(result.data.attributes.name)
+                            .then(function (translation) {
+                                if (!$scope.job.attributes.name_translation) {
+                                    $scope.job.attributes.name_translation = {};
+                                }
+                                $scope.job.attributes.name_translation.text = translation.translatedText;
+                                $scope.job.attributes.name_translation.from = translation.detectedSourceLanguage;
+                                $scope.job.attributes.name_translation.from_name = translation.detectedSourceLanguageName;
+                                $scope.job.attributes.name_translation.from_direction = translation.detectedSourceLanguageDirection;
+                                $scope.job.attributes.name_translation.to = translation.targetLanguage;
+                                $scope.job.attributes.name_translation.to_name = translation.targetLanguageName;
+                                $scope.job.attributes.name_translation.to_direction = translation.targetLanguageDirection;
+                            });
+                    }
+                    if (result.data.attributes.description) {
+                        gtService.translate(result.data.attributes.description)
+                            .then(function (translation) {
+                                if (!$scope.job.attributes.description_translation) {
+                                    $scope.job.attributes.description_translation = {};
+                                }
+                                $scope.job.attributes.description_translation.text = translation.translatedText;
+                                $scope.job.attributes.description_translation.from = translation.detectedSourceLanguage;
+                                $scope.job.attributes.description_translation.from_name = translation.detectedSourceLanguageName;
+                                $scope.job.attributes.description_translation.from_direction = translation.detectedSourceLanguageDirection;
+                                $scope.job.attributes.description_translation.to = translation.targetLanguage;
+                                $scope.job.attributes.description_translation.to_name = translation.targetLanguageName;
+                                $scope.job.attributes.description_translation.to_direction = translation.targetLanguageDirection;
+                            });
                     }
                 });
 
@@ -544,51 +590,95 @@ angular.module('just.common')
 
             i18nService.addLanguageChangeListener(function () {
                     that.getComments($routeParams.id);
+                    that.getJobData();
                 }
             );
 
-            $scope.jobb = jobService.getJob($routeParams.id, 'company,hourly-pay');
-            $scope.jobb.$promise.then(function (response) {
-                $scope.job = response.data;
-                $scope.job.company = response.included[0];
-                $scope.job.company_image = "assets/images/content/placeholder-logo.png";
+            //handle different dynamic translations
+            $scope.dt = {
+                job_item: true,
+                joblist_item: true
+            };
+            this.toggleDT = function (textId) {
+                $scope.dt[textId] = !$scope.dt[textId];
+            };
 
-                var found_hourly_pay = $filter('filter')(response.included, {
-                    id: "" + response.data.relationships["hourly-pay"].data.id,
-                    type: "hourly-pays"
-                }, true);
+            this.getJobData = function(){
+                Resources.job.get({
+                    id: $routeParams.id,
+                    "include": "owner,company,hourly-pay,job-users"
+                }, function (result) {
+                    $scope.isCompany = 0;
+                    $scope.job = result.data;
+                    $scope.job.owner = result.included[0];
+                    $scope.job.company = result.included[1];
+                    $scope.job.max_rate = result.included[2].attributes["gross-salary"];
+                    /*if ($scope.$parent) {
+                     $scope.job.max_rate = (($scope.$parent.ctrl.isCompany === 1) ? result.included[2].attributes["rate-excluding-vat"] : result.included[2].attributes["gross-salary"]);
+                     }*/
+                    $scope.job.totalRate = $scope.job.attributes.hours * $scope.job.max_rate;
+                    $scope.job.currency = result.included[2].attributes.currency;
+                    $scope.job.netRate = result.included[2].attributes["net-salary"] * $scope.job.attributes.hours;
+                    $scope.job.company_image = "assets/images/content/placeholder-logo.png";
 
-                if (found_hourly_pay.length > 0) {
-                    $scope.job.hourly_pay = found_hourly_pay[0].attributes;
-                    $scope.job.max_rate = found_hourly_pay[0].attributes["gross-salary"] * response.data.attributes.hours;
-                }
-
-                var company_image_arr = response.included[0].relationships["company-images"].data;
-                if (company_image_arr.length > 0) {
-                    var getCompany = companyService.getCompanyById(response.data.relationships.company.data.id);
-                    if (getCompany) {
-                        var found_image = $filter('filter')(getCompany.included, {type: 'company-images'}, true);
-                        if (found_image) {
-                            if (found_image.length > 0) {
-                                $scope.job.company_image = found_image[0].attributes["image-url-small"];
-                            }
-                        }
-                    } else {
-                        Resources.company.get({
-                            company_id: response.data.relationships.company.data.id,
-                            'include': 'company-images'
-                        }, function (result0) {
-                            var found_image = $filter('filter')(result0.included, {type: 'company-images'}, true);
+                    var company_image_arr = result.included[1].relationships["company-images"].data;
+                    if (company_image_arr.length > 0) {
+                        var getCompany = companyService.getCompanyById(result.data.relationships.company.data.id);
+                        if (getCompany) {
+                            var found_image = $filter('filter')(getCompany.included, {type: 'company-images'}, true);
                             if (found_image) {
                                 if (found_image.length > 0) {
                                     $scope.job.company_image = found_image[0].attributes["image-url-small"];
                                 }
                             }
-                            companyService.addList(result0);
-                        });
+                        } else {
+                            Resources.company.get({
+                                company_id: result.data.relationships.company.data.id,
+                                'include': 'company-images'
+                            }, function (result0) {
+                                var found_image = $filter('filter')(result0.included, {type: 'company-images'}, true);
+                                if (found_image) {
+                                    if (found_image.length > 0) {
+                                        $scope.job.company_image = found_image[0].attributes["image-url-small"];
+                                    }
+                                }
+                                companyService.addList(result0);
+                            });
+                        }
                     }
-                }
-            });
+
+                    if (result.data.attributes.name) {
+                        gtService.translate(result.data.attributes.name)
+                            .then(function (translation) {
+                                if (!$scope.job.attributes.name_translation) {
+                                    $scope.job.attributes.name_translation = {};
+                                }
+                                $scope.job.attributes.name_translation.text = translation.translatedText;
+                                $scope.job.attributes.name_translation.from = translation.detectedSourceLanguage;
+                                $scope.job.attributes.name_translation.from_name = translation.detectedSourceLanguageName;
+                                $scope.job.attributes.name_translation.from_direction = translation.detectedSourceLanguageDirection;
+                                $scope.job.attributes.name_translation.to = translation.targetLanguage;
+                                $scope.job.attributes.name_translation.to_name = translation.targetLanguageName;
+                                $scope.job.attributes.name_translation.to_direction = translation.targetLanguageDirection;
+                            });
+                    }
+                    if (result.data.attributes.description) {
+                        gtService.translate(result.data.attributes.description)
+                            .then(function (translation) {
+                                if (!$scope.job.attributes.description_translation) {
+                                    $scope.job.attributes.description_translation = {};
+                                }
+                                $scope.job.attributes.description_translation.text = translation.translatedText;
+                                $scope.job.attributes.description_translation.from = translation.detectedSourceLanguage;
+                                $scope.job.attributes.description_translation.from_name = translation.detectedSourceLanguageName;
+                                $scope.job.attributes.description_translation.from_direction = translation.detectedSourceLanguageDirection;
+                                $scope.job.attributes.description_translation.to = translation.targetLanguage;
+                                $scope.job.attributes.description_translation.to_name = translation.targetLanguageName;
+                                $scope.job.attributes.description_translation.to_direction = translation.targetLanguageDirection;
+                            });
+                    }
+                });
+            };
 
             this.getComments = function (job_id) {
                 $scope.commentss = commentService.getComments('jobs', job_id, 'owner,owner.user-images');
@@ -674,6 +764,7 @@ angular.module('just.common')
             };
 
             this.getComments($routeParams.id);
+            this.getJobData();
 
             this.submit = function () {
                 that.model.data.attributes["language-id"] = parseInt(i18nService.getLanguage().$$state.value.id);
