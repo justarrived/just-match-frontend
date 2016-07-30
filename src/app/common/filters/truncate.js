8e26a3f66ef38407c13036605987c8d345f06e75
Per Stenström <per.stenstrom@gmail.com>
2016-07-30 13:29:43 +0200

    #710 Fix type-o and truncate filter

--- src/app/common/filters/truncate.js
+++ src/app/common/filters/truncate.js
@@ -1,9 +1,7 @@
 angular.module('just.truncate', [])
     .filter('truncate', function () {
         return function(string, length) {
-            var 
-                parts = [],
-                truncatedString = '';
+            var truncatedString = '';
 
             if(string.length <= length) {
                 return string;
@@ -13,7 +11,7 @@ angular.module('just.truncate', [])
 
             // Don't end on puncuation or whitespace
             if([' ', '.', ',', '!', '?'].indexOf(truncatedString.substr(truncatedString.length - 1)) > -1) {
-                truncatedString.substr(0, truncatedString.length - 1);
+                truncatedString = truncatedString.substr(0, truncatedString.length - 1);
             }
 
             truncatedString += '…';
